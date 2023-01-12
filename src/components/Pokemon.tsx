56f21004fe5df78e1791a50f7911d568161a81cd
CatInPajamas <gaurabkhatry@gmail.com>
2023-01-12 18:45:22 +0545

    better images

--- src/components/Pokemon.tsx
+++ src/components/Pokemon.tsx
@@ -1,21 +1,5 @@
-import { useCallback, useEffect, useState, useRef } from "react";
-
-
-// const loadImage = async (id: Number) => {
-//     const res = await fetch(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`);
-//     const imageBlob = await res.blob();
-//     const imageObjectURL = URL.createObjectURL(imageBlob);
-//     return imageObjectURL;
-// }
-
-// const ImageComponent = async ({ id }) => {
-
-
-//     const img = loadImage(id);
-
-//     return <img src={img} />
-
-// }
+import { useCallback, useState} from "react";
+import "./Pokemon.css"
 
 export default function Pokemon() {
 
@@ -33,9 +17,18 @@ export default function Pokemon() {
 
     return (
         <div>
-            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`} />
-            <button onClick={vote} disabled={disabled}>Smash</button>
-            <button onClick={() => { setDisabled(false); setId(Math.ceil(Math.random() * 10)) }} disabled={disabled}>Pass</button>
+            
+            <div className="flex content-center col-span-2">
+            <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`} />
+            </div>
+            <div className="flex content-center gap-16">
+            <div className="flex">
+            <button onClick={vote} disabled={disabled} className="bg-[#FFC8DD] rounded-full hover:bg-[#FFAFCC] content-center"></button>
+            </div>
+            <div className="flex">
+            <button onClick={() => { setDisabled(false); setId(Math.ceil(Math.random() * 10)) }} disabled={disabled} className="rounded-full bg-[#BDE0FE] hover:bg-[#A2D2FF] content-center"></button>
+            </div>
+            </div>
         </div>
 
     )
